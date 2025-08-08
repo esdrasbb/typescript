9cb189b37ebca95e7dbdb8edbed6c564aa27ad62
Esdras Barreto <esdras.barreto@gmail.com>
2025-08-08 16:35:35 -0300

    Union type

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
 
-//alias
-type Users = {
-  name: string
-  age: number
+function combineInputs (input1 : number | string, input2 : number | string) {
+  let result
+  if (typeof input1 === 'number' && typeof input2 === 'number')
+    result = input1 + input2
+  else
+    result = `${input1}${input2}`
+  return result
 }
 
-const user2 : Users = {
- name: 'Foo',
- age: 35 
-}
-
-console.log (user2)
+console.log(combineInputs(10, 20))
+console.log(combineInputs('Apple', 'Orange'))
